02b55cd84a2fe43ff68219a0a1ac50e7eb0c90ca
Mackenzie-Clarkson <Mackenzie.Clarkson@ibm.com>
2019-05-13 15:05:07 -0400

    dply

--- src/api/projects.js
+++ src/api/projects.js
@@ -2,7 +2,7 @@ const projects =  [
   {
     title: 'Portfolio Website',
     desc: `You're on it! A modern SPA built with Vue and Vuetify. `,
-    featuredImage: 'static/stack/vue.jpeg',
+    featuredImage: '/portfolio/static/stack/vue.jpeg',
     author: 'Mackenzie Clarkson',
     createdAt: '02/03/2019',
     link: '#/site'
@@ -14,7 +14,7 @@ const projects =  [
     This app was a challenge in optmization as it is key to be able to run on top of a live video feed input.
     If the algorithm is too slow it would not be able to catch up to the frames a live feed provides. Two versions were made:
     one using open source libraries such as OPENCV and one without help (other than numpy).`,
-    featuredImage: 'static/stack/motion.gif',
+    featuredImage: '/portfolio/static/stack/motion.gif',
     author: 'Mackenzie Clarkson',
     createdAt: '04/01/2017',
     link: 'https://github.com/MackenzieClarkson/Motion-Detection--No-OpenCV-'
@@ -25,7 +25,7 @@ const projects =  [
     relevant data updates, posting and realtime updating of tasks for team collaboration. Also used by management for metrics like Inventory, shipping status, cancellations
     costs, and project management e.x resource allocation by project or client. My contribution was to rebuild this App from the ground up using a contemporary stack (From PHP based to Node/Vue/GraphQL)
     while also implementing the new design standards and the new CSS and HTML template from IBM. This App is internal and therefore private :(`,
-    featuredImage: 'static/stack/ibm.png',
+    featuredImage: '/portfolio/static/stack/ibm.png',
     author: 'CSCI Team @ IBM'
   },
 
